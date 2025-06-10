86b70f219db7701a18e6555b5ac7a3aea4887a66
hasindu lakshan <hasindulakshan0040@gmail.com>
2025-06-11 00:00:47 +0530

    home page and userAuth

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -10,15 +10,15 @@ const Hero = () => {
       <p className="bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20">
         The Ultimate Hotel Experience
       </p>
-      <h1 className="font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4">
-        Discover Your Perfect Gateway Destination
+      <h1 className="font-playfair text-2xl md:text-5xl md:text-[50px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4">
+        Discover Your Perfect Gateway Destinations
       </h1>
       <p className="max-w-130 mt-2 text-sm md:text-base">
         Unparalleled luxury and comfort await at the world's most exclusive
         hotels and resorts. Start your journey today.
       </p>
 
-      <form className="bg-white text-gray-500 rounded-lg px-6 py-4  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto">
+      <form className="bg-white text-gray-500 rounded-lg px-6 py-4 mt-8 flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto">
         <div>
           <div className="flex items-center gap-2">
             <img src={assets.calenderIcon} alt="" className="h-4" />
