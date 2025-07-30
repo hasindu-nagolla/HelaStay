f2b6fdc1485bb86f976e31504615e1025babfa58
hasindu lakshan <hasindulakshan0040@gmail.com>
2025-07-30 09:53:56 +0530

    create footer section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import FeaturedDestinations from "../components/FeaturedDestinations";
 import ExclusiveOffers from "../components/ExclusiveOffers";
 import Testimonial from "../components/Testimonial";
 import NewsLetter from "../components/NewsLetter";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
