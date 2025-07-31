ea779ae0e71976541729d0a5aeeb0c132338f45f
hasindu lakshan <hasindulakshan0040@gmail.com>
2025-07-31 21:26:27 +0530

    allrooms page

---
+++ client/src/pages/AllRooms.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AllRooms = () => {
+  return (
+    <div className="flex flex-col-reverse lg:fles-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24">
+      <div>
+        <div>
+          <h1>Hotel Rooms</h1>
+          <p>
+            take advantage of our limited time offers and special pkgs to enhnce
+            ur sty and create infrogotble memos
+          </p>
+        </div>
+      </div>
+      {/* Filters */}
+      <div></div>
+    </div>
+  );
+};
+
+export default AllRooms;
